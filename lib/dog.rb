58844beea24f88c34ecfa025ca61a5f4c240cf54
Ian Hutson <ih00209@georgiasouthern.edu>
2020-07-09 15:50:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,4 +7,15 @@ attr_accessor :id, :name, :breed
     @breed = breed
   end
   
+  def self.create_table
+    sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        grade TEXT
+        )
+    SQL
+    DB[:conn].execute(sql) 
+  end
+  
 end
