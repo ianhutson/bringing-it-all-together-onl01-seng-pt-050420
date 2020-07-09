4fb7b9513a7ed12541eb5f8a1ca1da11fef9323b
Ian Hutson <ih00209@georgiasouthern.edu>
2020-07-09 16:05:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -79,4 +79,15 @@ attr_accessor :id, :name, :breed
       self.new_from_db(row)
     end.first
   end
+  
+   def self.find_or_create_by(name:, breed:)
+    song = DB[:conn].execute("SELECT * FROM songs WHERE name = ? AND album = ?", name, album)
+    if !song.empty?
+      song_data = song[0]
+      song = Song.new(song_data[0], song_data[1], song_data[2])
+    else
+      song = self.create(name: name, album: album)
+    end
+    song
+  end
 end
