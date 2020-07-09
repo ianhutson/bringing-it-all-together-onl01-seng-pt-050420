a816b3d0791f1600b8ff7121547d00e0d183599d
Ian Hutson <ih00209@georgiasouthern.edu>
2020-07-09 16:03:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -66,4 +66,17 @@ attr_accessor :id, :name, :breed
       self.new_from_db(row)
     end.first
   end
+  
+  def self.find_by_name(id)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql,name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
 end
