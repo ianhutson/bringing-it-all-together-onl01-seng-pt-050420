30ba1639f5260d3aa70a11eee7e1b52ac379c30c
Ian Hutson <ih00209@georgiasouthern.edu>
2020-07-09 15:54:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -39,5 +39,6 @@ attr_accessor :id, :name, :breed
       DB[:conn].execute(sql, self.name, self.breed)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
     end
+    self
   end
 end
