a0f722c60e415bc06525262c909c5fc7e33956ed
Ian Hutson <ih00209@georgiasouthern.edu>
2020-07-09 16:02:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -54,13 +54,11 @@ attr_accessor :id, :name, :breed
     self.new(id: id, name: name, breed: breed)
   end
   
-  def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+  def self.find_by_id(id)
     sql = <<-SQL
       SELECT *
-      FROM students
-      WHERE name = ?
+      FROM dogs
+      WHERE id = ?
       LIMIT 1
     SQL
 
